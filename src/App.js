e659265351f51b987843292314749c4fe613f29f
Mau Reyes <reyes.marq@gmail.com>
2019-06-18 10:02:36 -0600

    initial commit base structure and writing some todos

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>CRUD HOOKS</p>
       </header>
+      <div className="container">
+        <h2 className="text-center">Todo</h2>
+        <ul className="list-group">
+          <li className="list-group-item">Buy some clothes</li>
+          <li className="list-group-item">Write some code</li>
+          <li className="list-group-item">Watch netflix</li>
+        </ul>
+      </div>
     </div>
   );
 }
